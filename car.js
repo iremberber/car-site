4dca34d3c2ebfc81c2b9d4d7559d35937d8e9f58
iremberber <iremberber0@gmail.com>
2024-01-30 00:30:07 +0300

    +update

--- car.js
+++ car.js
@@ -1,3 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const thirdrow = document.getElementsByClassName("row third-row")[0];
+  const thirdrowimg = document.getElementsByClassName("third-img")[0];
+  const forthrow = document.getElementsByClassName("forth-row")[0];
+  const slider = document.getElementsByClassName("slideshow-container")[0];
+  const sliderdots = document.getElementsByClassName("dot");
+
+  const button1 = document.getElementById("model1");
+  const button2 = document.getElementById("model2");
+  const button3 = document.getElementById("model3");
+  const button4 = document.getElementById("model4");
+
+  button1.addEventListener("click", function () {
+    thirdrow.style.display = "flex";
+    forthrow.style.display = "flex";
+    slider.style.display = "flex";
+
+    // Iterate through the sliderdots collection to set display individually
+    for (let i = 0; i < sliderdots.length; i++) {
+      sliderdots[i].style.display = "flex";
+    }
+  });
+});
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
